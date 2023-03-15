4bded28c9bb20db2a6b44f8f7c2df41a536a901e
Muhammad Usama <muhammadsprofile@gmail.com>
2023-03-16 04:58:42 +0500

    Complete 5 MCQs Function

---
+++ script.js
@@ -0,0 +1,106 @@
+const quizData = [
+    {
+        question: "What is the national animal of Pakistan?",
+        a: "Goat",
+        b: "Sheep",
+        c: "Markhor",
+        d: "Eagle",
+        correct: "c",
+    },
+    {
+        question: "What is the national game of Pakistan?",
+        a: "Cricket",
+        b: "Hockey",
+        c: "Tennis",
+        d: "Football",
+        correct: "b",
+    },
+    {
+        question: "What is the national language of Pakistan?",
+        a: "Urdu",
+        b: "English",
+        c: "Punjabi",
+        d: "Sindhi",
+        correct: "a",
+    },
+    {
+        question: "What is the capital city of Pakistan?",
+        a: "Lahore",
+        b: "Karachi",
+        c: "Multan",
+        d: "Islamabad",
+        correct: "d",
+    },
+    {
+        question: "What is the national fruit of Pakistan?",
+        a: "Banana",
+        b: "Apple",
+        c: "Strawberry",
+        d: "Mango",
+        correct: "d",
+    },
+
+];
+
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll ('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if (currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+            <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
+})
